f89e9f472fcbc0d4914272da24fd0215ab85e5df
gzguoer <gzguoer@a451cfe6-3093-11de-9e80-b52b74bdfc52>
2009-05-11 09:27:23 +0000

    MOD:add xhtml feature to editor

--- app/admin.py
+++ app/admin.py
@@ -3,8 +3,10 @@ import os
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
+from google.appengine.ext import db
 
 from app import util
+from model import Post
 
 class MainPage(webapp.RequestHandler):
   def get(self):      
@@ -14,12 +16,17 @@ class MainPage(webapp.RequestHandler):
     else:
       url = util.xhtmlize_url(users.create_login_url(self.request.uri))
       url_linktext = 'Login'
+    posts = Post.gql('ORDER BY date desc')
+    post = None
+    if posts and posts[0]:
+      post=posts[0]
     values = {
       'user': users.GetCurrentUser(),
       'user_is_admin': users.is_current_user_admin(),
       'user_nickname': util.getUserNickname(users.get_current_user()),
       'url': url,
       'url_linktext': url_linktext,
+      'post': post,
     }    
     path = os.path.join(os.path.dirname(__file__),'../templates/admin.html')
     self.response.out.write(template.render(path, values))
