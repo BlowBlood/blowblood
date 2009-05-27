b5763032f639c76ce549d7aef262e9812eae2cbe
gzguoer <gzguoer@a451cfe6-3093-11de-9e80-b52b74bdfc52>
2009-05-27 11:53:01 +0000

    MOD: change authorized module

--- app/authorized.py
+++ app/authorized.py
@@ -27,15 +27,15 @@ def role(role):
             user = users.get_current_user()
             if not user:
                 if self.request.method != 'GET':
-                    self.response.out.write('403!Not Authorized')
+                    self.error(403)
                 else:
                     self.redirect(users.create_login_url(self.request.uri))
             elif role == "user" or (role == "admin" and users.is_current_user_admin()):
                 return handler_method(self, *args, **kwargs)
             else:
                 if self.request.method == 'GET':
-                    self.redirect("/403.html")
+                    self.redirect("/403.html")  # Some unauthorized feedback
                 else:
-                    self.response.out.write('403!Not Authorized') # User didn't meet role.
+                    self.error(403) # User didn't meet role.  
         return check_login
     return wrapper
