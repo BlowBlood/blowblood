a05e5ee8e291e6e12cefa4163bad58bb2f6212f1
gzguoer <gzguoer@a451cfe6-3093-11de-9e80-b52b74bdfc52>
2009-05-27 13:23:49 +0000

    MOD: avoid logging admin

--- app/log.py
+++ app/log.py
@@ -18,13 +18,13 @@ def counter(handler_method):
   
 def visitor(handler_method):
   def wrapper(self, *args, **kwargs):
-    visitor = Visitor()
-    visitor.nickname= os.environ['USER_EMAIL']
-    visitor.userIP = os.environ['REMOTE_ADDR']
-    visitor.url = os.environ['PATH_INFO']
-    visitor.webbrowser = os.environ['HTTP_USER_AGENT']
-    if os.environ.has_key('HTTP_REFERER'):
-      visitor.referer = os.environ['HTTP_REFERER']    
-    visitor.put()
+    if (os.environ.get('USER_IS_ADMIN', '0')) != '1':
+      visitor = Visitor()
+      visitor.nickname= os.environ.get('USER_EMAIL','')
+      visitor.userIP = os.environ.get('REMOTE_ADDR','')
+      visitor.url = os.environ.get('PATH_INFO','')
+      visitor.webbrowser = os.environ.get('HTTP_USER_AGENT','')
+      visitor.referer = os.environ.get('HTTP_REFERER','')
+      visitor.put()
     handler_method(self, *args, **kwargs)
   return wrapper
