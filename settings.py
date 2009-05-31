cbb7be82e60e5c91312c6dce64c20f815469ad0d
gzguoer <gzguoer@a451cfe6-3093-11de-9e80-b52b74bdfc52>
2009-05-31 12:22:31 +0000

    MOD: add global setting file and add default expiration with 10days

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Settings for my project.
+
+#####################
+#  Global Settings  #
+#####################
+RUN_DEBUG = True
+
+BLOG_TITLE = 'BlowBlood'
+BLOG_DESCRIPTION = 'BlowBlood@WestGate'
+BLOG_URI = 'http://www.blowblood.com'
+
+#####################
+#  Site Settings    #
+#####################
+
+PAGESIZE = 8
